63ea36ea008dde718b2114024098f45e2d40efaf
Daniel Vive <dafi0001@stud.kea.dk>
2024-09-24 13:56:33 +0200

    initial commit

---
+++ src/main/java/com/example/demogithubact/DemoGitHubActApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogithubact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitHubActApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitHubActApplication.class, args);
+    }
+
+}
